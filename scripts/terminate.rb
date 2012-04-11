3b4b37859855d01897bc4ed29c592b6012cdf0c6
Brian Jakovich <b.jakovich@gmail.com>
2012-04-11 12:48:05 -0400

    added stackname

--- scripts/terminate.rb
+++ scripts/terminate.rb
@@ -2,15 +2,13 @@ require 'rubygems'
 require 'aws-sdk'
 
 
-que_name=ARGV[0]
+instance=ARGV[0]
 access_key=ARGV[1]
 secret_access_key=ARGV[2]
 
-sqs = AWS::SQS.new(
+ec2 = AWS::EC2.new(
   :access_key_id => "#{access_key}",
   :secret_access_key => "#{secret_access_key}")
 
-queue = sqs.queues.named("#{que_name}")
-
-status = queue.receive_message(:limit => 1)
-puts status.body
+instances = ec2.instances.ip_address("#{instance}")
+puts instances.status
